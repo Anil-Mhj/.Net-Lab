d379a67426675d957888f0cc654e0152b97fa793
Anil-Mhj <maharjananil377@gmail.com>
2026-07-29 20:13:19 +0545

    Completed upto Lab 12

---
+++ Lab5/Program.cs
@@ -0,0 +1,31 @@
+﻿// Create a Calculator class and overload the Add() method to perform addition of two integers, two double values, and
+
+// three integers. Call each overloaded method from the Main() method and display the results to demonstrate compile-
+// time polymorphism.
+
+using System;
+
+namespace Lab5{
+    public class Calculator{
+        public int Add(int a, int b){
+            return a+b;
+        }
+
+        public double Add(double a, double b){
+            return a+b;
+        }
+
+        public int Add(int a, int b, int c){
+            return a+b+c;
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Calculator calc = new Calculator();
+            Console.WriteLine($"Sum of two integers: {calc.Add(1,8)}");
+            Console.WriteLine($"Sum of two doubles: {calc.Add(3.8,5.1)}");
+            Console.WriteLine($"Sum of three integers: {calc.Add(4,8,9)}");
+        }
+    }
+}
---
+++ Lab8/Program.cs
@@ -0,0 +1,57 @@
+﻿// Create a base class named Shape with a virtual method called Draw(). Derive the classes Circle, Rectangle, and
+// Triangle, and override the Draw() method in each class to display an appropriate message indicating which shape is
+// being drawn. Demonstrate method overriding by invoking the method using base class references.
+
+using System;
+
+namespace Lab8
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape!");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle!");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle!");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Triangle!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape;
+
+            shape = new Circle();
+            shape.Draw();
+
+            shape = new Rectangle();
+            shape.Draw();
+
+            shape = new Triangle();
+            shape.Draw();
+        }
+    }
+}
