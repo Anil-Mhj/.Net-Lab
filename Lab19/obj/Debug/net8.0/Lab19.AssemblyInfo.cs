bd3323e0f6ac15c6b055a0e0f693beb4c235386c
Anil-Mhj <maharjananil377@gmail.com>
2026-08-05 19:28:08 +0545

    Lab completed upto Lab 22

--- Lab19/obj/Debug/net8.0/Lab19.AssemblyInfo.cs
+++ Lab19/obj/Debug/net8.0/Lab19.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab19")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfc795ebfe3b99d050094214dddd68c2ed1f5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24a4308542e2e07104188e8a72692554235002a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab19")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab19")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
