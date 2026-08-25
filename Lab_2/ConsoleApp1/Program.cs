28c5da1d99f26d0770cbf47a708b71dbc6fd70a4
Anil-Mhj <maharjananil377@gmail.com>
2026-08-25 07:57:43 +0545

    Completed Lab 2

---
+++ Lab_2/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Create a Generic Class named DataStorage<T> that can store and display values of different
+// data types such as integer, string, and double. Create objects of the generic class for each data
+// type and demonstrate that the same class can be reused for different types.
+
+using System;
+
+namespace ConsoleApp1{
+    internal class DataStorage<T>{
+        public T Data;
+
+        public DataStorage(T data){
+            this.Data = data;
+        }
+
+        public void Display(){
+            Console.WriteLine($"Data: {this.Data}");
+        }
+    }
+    
+    internal class Program{
+        static void Main(string[] args){
+            DataStorage<string> ds1 = new DataStorage<string>("Hello");
+            DataStorage<int> ds2 = new DataStorage<int>(42);
+            DataStorage<double> ds3 = new DataStorage<double>(50.45);
+            ds1.Display();
+            ds2.Display();
+            ds3.Display();
+        }
+    }
+}
